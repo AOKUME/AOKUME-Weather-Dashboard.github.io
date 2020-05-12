7d889c6fe780604bf016373c6316b709bf460e56
Jonathan Sarmiento <john.sarmiento@me.com>
2020-05-12 01:26:33 -0500

    Updated HTML/CSS/JS
    - Updated HTML
    - Added CSS Styling
    - Added Website Logo
    - Connected JS with Open Weather API
    - Working on getting icon to show up
    - Will link the remaining JS content to its appropriate HTML element next session.
    - Added Loading GIF in background and will distribute accordingly to all HTML values that load on page refresh.

--- Assets/index.js
+++ Assets/index.js
@@ -4,26 +4,20 @@ var APIKey = "7b7da8f7f0400b14633ffa73f109e78d";
 // Here we are building the URL we need to query the database
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey + '&units=imperial';
 
-// We then created an AJAX call
+// We then created an AJAX call //
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
-//    console.log(response);
-//    $(".city").html(`<h1>${response.name}</h1>`);
-    $('.wind').html(response.wind.speed);
-    $('.humidity').html(response.main.humidity);
-    $('.temp').html(response.main.temp);
-    $('.temp').html(response.main.temp);
-    // Create CODE HERE to Log the queryURL
-    // Create CODE HERE to log the resulting object
-    // Create CODE HERE to calculate the temperature (converted from Kelvin)
-    // Create CODE HERE to transfer content to HTML
-    // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-    // function temperatureConverter(valNum) {
-    //     valNum = parseFloat(valNum);
-    //     document.getElementById("outputFahrenheit").innerHTML = ((valNum - 273.15) * 1.8) + 32;
-    // }
-    // Create CODE HERE to dump the temperature content into HTML
 
+console.log(response) //
+    $(".city").html(response.name);
+    $('.wind').html("Wind: " + response.wind.speed);
+    $('.humidity').html("Humidity: " + response.main.humidity);
+    $('.temp').html("temp: " + response.main.temp);
+    $('.temp').html("temp: " + response.main.temp);
+    $('.desc').html(response.weather[0].description);
+    // $('.ico').html(response.weather[0].icon)
+
+    $('.ico').html(`<br><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}" alt="weather icon">`)
 });
